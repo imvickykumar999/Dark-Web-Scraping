9bb3d301db3479098810883aba65f115e1c5b724
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-11 10:57:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 
 # from HostTor import VicksTor
-import VicksTor as vix
-vix.run_server('flask')
+# import VicksTor as vix
+# vix.run_server('flask')
 
 from bs4 import BeautifulSoup as bs
 from flask import Flask
 import requests
 
-link = 'https://haveibeenpwned.com/PwnedWebsites'
-req = requests.get(link)
+# link = 'https://172.217.204.100/'                  # verify=False
+link = 'https://imvickykumar999.pythonanywhere.com/' # verify=True
+req = requests.get(link, verify=True)
 
 soup = bs(req.content, 'html5lib')
 app = Flask(__name__)
